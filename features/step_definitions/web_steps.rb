a8de25af7975aad8fc8436bcfe97f7ef54b30caf
yoitsukamijo <yoitsu@berkeley.edu>
2016-03-05 05:18:42 +0000

    added steps

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -24,6 +24,8 @@ require 'cgi'
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "selectors"))
 
+@temp_user
+
 module WithinHelpers
   def with_scope(locator)
     locator ? within(*selector_for(locator)) { yield } : yield
@@ -43,6 +45,34 @@ end
 
 Given /^(?:|I )am on (.+)$/ do |page_name|
   visit path_to(page_name)
+end
+
+Given(/^the following users exist:$/) do |table|
+  table.hashes.each do |user|
+    User.create(:username => user[:username], :password => user[:password])
+  end
+end
+
+When(/^I fill in user with "([^"]*)"$/) do |arg1|
+  @temp_user = User.create(:usernam => arg1)
+end
+
+When(/^I fill in password with "([^"]*)"$/) do |arg1|
+  @temp_user.update_attribute(:password => arg1)
+end
+
+Given(/^the following patients exist:$/) do |table|
+  table.hashes.each do |patient|
+    Patient.create(:name => patient["Name"], :gender => patient["Gender"], :start_date => patient["Start Date"])
+  end
+end
+
+Then(/^I should (not\s+)?see "([^"]*)" before "([^"]*)"$/) do |no, arg1, arg2|
+  if no 
+    expect(page).not_to have_content(/#{arg1}.*#{arg2}.*/)
+  else
+    expect(page).to have_content(/#{arg1}.*#{arg2}.*/)
+  end
 end
 
 When /^(?:|I )go to (.+)$/ do |page_name|
