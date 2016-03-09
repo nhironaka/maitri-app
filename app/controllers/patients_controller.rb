837e1dbdcebea3ac22c7429230f14fa0f7799d7c
Hriday Kemburu <hkemburu@berkeley.edu>
2016-03-09 05:18:13 +0000

    Implemented patients_controller

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,27 @@
+class PatientsController < ApplicationController
+    
+    def patient_params
+        params.require(:patient).permit(:name, :gender, :start_date, :end_date)
+    end
+    
+    def index
+        @patients = Patient.all # or any other default handler which returns ActiveRecord::Relation
+        
+        sort = params[:sort] || session[:sort]
+        case sort
+            when 'name'
+              ordering, @name_header = {:name => :asc}, 'hilite'
+            when 'start_date'
+              ordering, @start_date_header = {:start_date => :asc}, 'hilite'
+            when 'end_date'
+              ordering, @end_date_header = {:end_date => :asc}, 'hilite'
+        end
+        
+        if params[:sort] != session[:sort]
+          session[:sort] = sort
+          redirect_to :sort => sort and return
+        end
+        @patients = @patients.order(ordering) if params[:sort].present? # OR if ordering
+    end
+    
+end
