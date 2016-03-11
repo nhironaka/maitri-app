e0fb634ecadd49b8455e359738a69753740628bf
lucianonly <peterli93@berkeley.edu>
2016-03-11 05:00:05 +0000

    Corrected some syntax errors

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -5,12 +5,6 @@ Given(/^the following users exist:$/) do |table|
   end
 end
 
-Given(/^the following patients exist:$/) do |table|
-  table.hashes.each do |patient|
-    Patient.create(:name => patient["Name"], :gender => patient["Gender"], :start_date => patient["Start Date"])
-  end
-end
-
 Then(/^I should (not\s+)?see "([^"]*)" before "([^"]*)"$/) do |no, arg1, arg2|
   if no 
     expect(page).not_to have_content(/#{arg1}.*#{arg2}.*/)
@@ -18,6 +12,5 @@ Then(/^I should (not\s+)?see "([^"]*)" before "([^"]*)"$/) do |no, arg1, arg2|
     expect(page).to have_content(/#{arg1}.*#{arg2}.*/)
   end
     User.create(:username => user[:username], :password => user[:password], :email => user[:email], :password_confirmation => user[:password_confirmation])
-  end
 end
 
