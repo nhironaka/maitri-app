670724521d489daa5415ee9694dd7a126f8d13f2
Naomi <Naomi.hironaka@berkeley.edu>
2016-04-10 05:34:31 +0000

    app doesn't error, but email not sent

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,19 +14,21 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'localhost:8080' }
-  config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.smtp_settings = {
-    :address => 'gmail.com',
-    :port => 587,
-    :domain => ENV["GMAIL_DOMAIN"],
-    :authentication => :login,
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  Rails.application.routes.default_url_options[:host] = 'hidden-savannah-29295.herokuapp.com'
+  ActionMailer::Base.delivery_method = :sendmail
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = true
+  
+  ActionMailer::Base.smtp_settings = {
+    :enable_starttls_auto => true,  
+    :address            => 'smtp.gmail.com',
+    :port               => 587,
+    :domain => 'gmail.com',
+    :authentication => 'plain',
     :user_name => ENV["GMAIL_USERNAME"],
     :password => ENV["GMAIL_PASSWORD"],
-    :tls                  => true,
-    :enable_starttls_auto => true 
+    :openssl_verify_mode => 'none'
   }
   
 
