54761212bf512cae62ec6152b658eeb54c9cc7d8
lucianonly <peterli93@berkeley.edu>
2016-04-11 21:24:53 +0000

    added heroku to mailer in application.rb

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module Maitri
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.action_mailer.default_url_options = { host: 'maitri-compassionate-care-app.herokuapp.com' }
   end
 end
