db5dc730047f09d075cb6889088716dc8eb651f5
lucianonly <peterli93@berkeley.edu>
2016-04-11 08:11:34 +0000

    sendmail instead of smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,11 +68,11 @@ Rails.application.configure do
   
   config.action_mailer.default_url_options = { :host => 'maitri-compassionate-care-app.herokuapp.com' }
   Rails.application.routes.default_url_options[:host] = 'maitri-compassionate-care-app.herokuapp.com'
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
-  
+
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
