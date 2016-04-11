2ecc787f322fe5b742f6f2a1976d3575dc2f04b8
Naomi <Naomi.hironaka@berkeley.edu>
2016-04-11 07:40:01 +0000

    updated production configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,20 +71,19 @@ Rails.application.configure do
   
   config.action_mailer.default_url_options = { :host => 'hidden-savannah-29295.herokuapp.com' }
   Rails.application.routes.default_url_options[:host] = 'hidden-savannah-29295.herokuapp.com'
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.perform_deliveries = true
-  ActionMailer::Base.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
   
-  ActionMailer::Base.smtp_settings = {
-    :enable_starttls_auto => true,  
-    :address            => 'smtp.gmail.com',
-    :port               => 587,
-    :domain => 'gmail.com',
-    :authentication => 'plain',
-    :user_name => ENV["GMAIL_USERNAME"],
-    :password => ENV["GMAIL_PASSWORD"],
-    :openssl_verify_mode => 'none',
-    :tls => true
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
   }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
