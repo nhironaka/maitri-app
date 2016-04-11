cfd8a3ab2974191c5f2747af899973c97331d35e
Naomi <Naomi.hironaka@berkeley.edu>
2016-04-11 07:49:46 +0000

    updated production, and removed uncalled functions

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,21 +2,21 @@
 #http://stackoverflow.com/questions/2817506/import-excel-into-rails-app
 #https://github.com/continuum/active_importer
 class Patient < ActiveRecord::Base
-    def self.import(file)
-        spreadsheet = open_spreadsheet(file)
-        header = spreadsheet.row(1)
-        (2..spreadsheet.last_row).each do |i|
-          row = Hash[[header, spreadsheet.row(i)].transpose]
-          patient = find_by_id(row["id"]) || new
-          patient.save!
-        end
-    end
+    #def self.import(file)
+     #   spreadsheet = open_spreadsheet(file)
+      #  header = spreadsheet.row(1)
+       # (2..spreadsheet.last_row).each do |i|
+        #  row = Hash[[header, spreadsheet.row(i)].transpose]
+         # patient = find_by_id(row["id"]) || new
+          #patient.save!
+        #end
+    #end
     
-    def self.open_spreadsheet(file)
-        Excel.new(file.path, nil, :ignore)
-    end
+    #def self.open_spreadsheet(file)
+     #   Excel.new(file.path, nil, :ignore)
+    #end
     
-    def self.all_headers
-        ["Name", "Gender", "Start Date", "End Date"]
-    end
+    #def self.all_headers
+     #   ["Name", "Gender", "Start Date", "End Date"]
+    #end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,13 +77,13 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
   
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => ENV["GMAIL_DOMAIN"],
+    :authentication => "plain",
+    :enable_starttls_auto => true,
+    :user_name => ENV["GMAIL_USERNAME"],
+    :password => ENV["GMAIL_PASSWORD"]
   }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
