e0c9b7c6c9ee5e8966ccf1f044fce9da483bfb85
Naomi <Naomi.hironaka@berkeley.edu>
2016-04-11 08:14:54 +0000

    changed to smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   
   config.action_mailer.default_url_options = { :host => 'hidden-savannah-29295.herokuapp.com' }
   Rails.application.routes.default_url_options[:host] = 'hidden-savannah-29295.herokuapp.com'
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
