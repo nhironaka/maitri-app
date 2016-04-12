0b93952aaa083f4fd8f5a46902e8167e1dfba813
Naomi <Naomi.hironaka@berkeley.edu>
2016-04-12 04:50:08 +0000

    updated production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,17 +73,17 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.default_options = {
-    from: ENV["SENDGRID_USERNAME"]
+    :from=> ENV["SENDGRID_USERNAME"]
   }
   config.action_mailer.smtp_settings = {
-  address: "smtp.sendgrid.net",
-  port: 587,
-  domain: "heroku.com", 
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: ENV["SENDGRID_USERNAME"],
-  password: ENV["SENDGRID_PASSWORD"],
-  :openssl_verify_mode => 'none'
+    :address=>"smtp.sendgrid.net",
+    :port=> 25,
+    :domain=> "heroku.com", 
+    :authentication=> "plain",
+    :enable_starttls_auto=> true,
+    :user_name=> ENV["SENDGRID_USERNAME"],
+    :password=> ENV["SENDGRID_PASSWORD"],
+    :openssl_verify_mode => 'none'
   }
   
   #config.action_mailer.default_url_options = { :host => 'hidden-savannah-29295.herokuapp.com' }
