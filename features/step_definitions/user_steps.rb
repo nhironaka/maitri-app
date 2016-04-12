d4a4b705a292b469988317e08818e887faab1c67
adityachalla <aditya_cj@berkeley.edu>
2016-04-12 02:51:45 +0000

    Patient Facesheets / profiles

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -13,6 +13,13 @@ Given /I am logged in/ do
   click_button "Log In"
 end
 
+Given /I am not logged in/ do
+  user = "existuser"
+  visit '/users/forgot_password'
+  fill_in "user_email", :with =>user
+  click_button "Send me reset password instructions"
+end
+
 Then(/^I should (not\s+)?see "([^"]*)" before "([^"]*)"$/) do |no, arg1, arg2|
   if no 
     expect(page).not_to have_content(/#{arg1}.*#{arg2}.*/)
