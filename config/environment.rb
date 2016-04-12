e14a56460355d51d71915f9b31767904002592b8
Naomi <Naomi.hironaka@berkeley.edu>
2016-04-12 04:17:25 +0000

    updated environemnt.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-config.action_mailer.delivery_method = :sendmail
+ActionMailer::Base.delivery_method = :sendmail
