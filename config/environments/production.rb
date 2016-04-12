740affa6a55997a7ec431c0e2a1106faedc09d24
lucianonly <peterli93@berkeley.edu>
2016-04-12 04:05:58 +0000

    Added figaro

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   
   config.action_mailer.smtp_settings = {
   address: "smtp.sendgrid.net",
-  port: 25,
+  port: 587,
   domain: "heroku.com", 
   authentication: "plain",
   enable_starttls_auto: true,
