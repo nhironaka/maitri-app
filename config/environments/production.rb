09540e3674832e5c0673cb95d0ba06849adb49cc
lucianonly <peterli93@berkeley.edu>
2016-04-12 00:40:25 +0000

    /usr/sbin/sendmail: not found

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,23 +66,22 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.delivery_method = :smtp
   
-  config.action_mailer.default_url_options = { :host => 'maitri-compassionate-care-app.herokuapp.com' }
-  Rails.application.routes.default_url_options[:host] = 'maitri-compassionate-care-app.herokuapp.com'
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
+  #config.action_mailer.default_url_options = { :host => 'maitri-compassionate-care-app.herokuapp.com' }
+  #Rails.application.routes.default_url_options[:host] = 'maitri-compassionate-care-app.herokuapp.com'
+  #config.action_mailer.delivery_method = :sendmail
+  #config.action_mailer.perform_deliveries = true
+  #config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.default :charset => "utf-8"
 
-  config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: "herokuapp.com",
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"],
-  :openssl_verify_mode => 'none'
-  }
+  config.action_mailer.default_url_options = { :host => 'maitri-compassionate-care-app.herokuapp.com' }
+  ActionMailer::Base.smtp_settings = {
+  :address        => "smtp.sendgrid.net",
+  :port           => "25",
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => ENV['SENDGRID_DOMAIN']
+}
   
   #config.action_mailer.default_url_options = { :host => 'hidden-savannah-29295.herokuapp.com' }
   #Rails.application.routes.default_url_options[:host] = 'hidden-savannah-29295.herokuapp.com'
