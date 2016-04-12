f239d318a0c15d6afedfb352fd3c9da0e26d163f
lucianonly <peterli93@berkeley.edu>
2016-04-12 04:08:42 +0000

    Added figaro

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,15 +77,15 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   
-  config.action_mailer.smtp_settings = {
-  address: "smtp.sendgrid.net",
-  port: 587,
-  domain: "heroku.com", 
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: ENV["SENDGRID_USERNAME"],
-  password: ENV["SENDGRID_PASSWORD"]
-  }
+  ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
   
   #config.action_mailer.default_url_options = { :host => 'hidden-savannah-29295.herokuapp.com' }
   #Rails.application.routes.default_url_options[:host] = 'hidden-savannah-29295.herokuapp.com'
