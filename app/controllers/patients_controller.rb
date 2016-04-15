cb7e8c9d12e3823e2c1009c92b57a3c724f0f769
yoitsukamijo <yoitsu@berkeley.edu>
2016-04-15 20:26:57 +0000

    added import page

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,12 +1,16 @@
 class PatientsController < ApplicationController
     helper_method :sort_column, :sort_direction
-    
     def patient_params
         params.require(:patient).permit(:name, :gender, :start_date, :end_date)
     end
     
     def show
-      id = params[:patient_id] 
+      import = params[:import]
+      id = params[:patient_id]
+      if import
+        render "import_view"
+        return
+      end
       @patient = Patient.find(id) 
     end
     
@@ -21,6 +25,18 @@ class PatientsController < ApplicationController
       end
       if not end_date.nil? and not end_date.empty?
         @patients = @patients.where('end_date < ?', end_date)
+      end
+    end
+    
+    def import_excel
+      Patient.delete_all
+      file = params[:file].path
+      patients_sheets = RubyXL::Parser.parse(file)
+      patients_sheets.each  do |patients_sheet| #starts on the second row
+        patients_sheet.each do |row|
+          name = row[1] + " " + row[3]
+          Patient.create(name: name)
+        end
       end
     end
       
