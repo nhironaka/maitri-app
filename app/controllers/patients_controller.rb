19effc05ae8b7171f3e59c3a026426fd9939e574
Naomi <Naomi.hironaka@berkeley.edu>
2016-04-23 19:50:37 +0000

    fixed reporting issue. TODO: create downloadable excel with data

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,14 +1,6 @@
 class PatientsController < ApplicationController
     helper_method :sort_column, :sort_direction
-    @@headers = ['Only_Active_Residents', 'First_Name',	'Middle_Initial',	'Last_Name', 'Nickname', 'Suffix',	'Marital_status',	
-               'Birthday', 'Gender', 'Race', 'Language',	'Religion',	'SSN',	'Medicare',
-               'Medicaid', 'Residency_Start',	'Residency_End', 'Apartment_Number',	'Phone',	'task_Rating', 'hospital_Preference',
-                'funeral_Home_Pref',	'pharmacy_Preference',	'pharmacy_Phone',	'Do_Not_Resuscitate',	'Health_Care_Power_of_Attorney', 'HCPOA_Activated?', 'Living_Will',	
-                'Advanced_directives',	'Allergies',	'Last_Pneumonia_shot',	'Last_flu_shot', 'Last TB Test',	'Insurance_1_Label',	'Insurance_1_Value',	
-                'Insurance_2_Label', 'Insurance_2_Value',	'Insurance_3_Label',	'Insurance_3_Value', 'Preferred_Pronouns',	'Admission_Viral_Load_Date',	'Admission_Viral_Load',	
-                'Admission_CD4_Count_Date',	'Admission_CD4_Count', 'Admission_Karnofsky_Score', 'Status_(ie:Respite/EOL/hospice)',	'Substance_Use_Hx',	'Criminal_Hx', 'Psychiatric_Dx',	
-                'Address_of_Permanent_Residence',	'Resident_Cell_Phone_number',	'Resident_Home_Phone_number_At_Permanent_Residence', 'AHP_Contract_Start_Date',	'AHP_Contract_End_Date', 'ADAP', 'Notes']
-    @@map = Hash[@@headers.zip [*1..56].collect{|val| val.to_words}]
+    include ApplicationHelper
     def patient_params
         params.require(:patient).permit(:two, :three, :sixteen, :seventeen)
     end
@@ -51,11 +43,11 @@ class PatientsController < ApplicationController
         if !filter.nil?
           k = @@map[filter]
           if cond == "less_than"
-            @patients = @patients.where("? < #{k}", val)
+            @patients = @patients.where("? > #{k}", "#{val}%")
           elsif cond == "greater_than"
-            @patients = @patients.where("? > #{k}", val)
+            @patients = @patients.where("#{k} > ?", "#{val}%")
           else 
-            @patients = @patients.where("? == #{k}", val)
+            @patients = @patients.where("#{k} LIKE ?", "#{val}%")
           end
         else
           return @patients
@@ -79,7 +71,7 @@ class PatientsController < ApplicationController
           Patient.create!(attrbs)
         end
       end
-      redirect_to patients_overview_path
+      redirect_to patients_overview_path(params)
     end  
     
     private
